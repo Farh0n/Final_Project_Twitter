8e3b6f73fa5a23f26918d5da25a3195cfb58b555
Farhan <farhanshirzadi@outlook.com>
2024-03-01 18:03:10 +0330

    completed the photouploader component

---
+++ Twitter_Front/src/components/PhotoUploader.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import '../styles/PhotoUploader.css';
+
+const PhotoUploader: React.FC = () => {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result;
+        if (result && typeof result === 'string') {
+          setImagePreview(result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div className="frame">
+      <div className="center">
+        <div className="title">Drop File to Upload</div>
+        <label htmlFor="file-input" className="file-label">
+          <span className="dropzone">
+            {imagePreview ? (
+              <img src={imagePreview} alt="Uploaded" className="preview-image" />
+            ) : (
+              <img src="https://100dayscss.com/codepen/upload.svg" alt="Upload Icon" />
+            )}
+          </span>
+          <input id="file-input" type="file" onChange={handleFileChange} required />
+        </label>
+        <button className="file-upload">Upload File</button>
+      </div>
+    </div>
+  );
+};
+
+export default PhotoUploader;
---
+++ Twitter_Front/src/components/Tweet/NewTweet.tsx
@@ -0,0 +1,9 @@
+
+
+
+export const NewTweet =()=> {
+   return <div>
+                <h2>Test</h2>
+            </div>
+   
+}
